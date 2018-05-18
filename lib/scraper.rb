748c1554003d8115e911d1be919dca52288f8571
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-18 23:23:40 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,8 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
-    Nokogiri::HTML(open())
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
     
   end 
   
